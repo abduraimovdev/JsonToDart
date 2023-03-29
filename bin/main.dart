8f9bcf1414e147cae6e0c54d06f5df086ea74400
abduraimovdev <kenzo778118@gmail.com>
2023-03-29 23:25:04 +0500

    Home task Succesfully

--- bin/main.dart
+++ bin/main.dart
@@ -1,67 +1,65 @@
 import 'dart:convert';
 import 'dart:io';
-// String pathGlobalDir = "C:\\Users\\User\\IdeaProjects\\myhomtask";
-// Directory directory = Directory(pathGlobalDir);
-// String pathLocalDir = "assets";
+
+import 'package:test/expect.dart';
+String pathGlobalDir = "C:\\Users\\abdu7\\OneDrive\\Desktop\\dart\\my_home_task\\assets";
+Directory directory = Directory(pathGlobalDir);
+String fileName = "readme.md";
 
 
 // task 1
-// void main() {
-// String pathLocalDir = "assets";
-// Directory directory = Directory(pathLocalDir);
+// void main() async{
+// Directory directory = await Directory(pathGlobalDir).create();
 // print(directory);
+// }
+//
+
+
+// task 2
+// void main() {
+//   Directory directory = Directory(pathGlobalDir);
+//   directory.parent.list().forEach(print);
 // }
 
 
 
-// task 2
-//   void main() {
-// Directory directory = Directory(pathGlobalDir);
-//   print("directory.parent: ${directory.parent.path}");
-//   }
 
 
 
 // task 3
 // void main(){
-//   Directory newDirectory = Directory("$pathLocalDir/dart");
-//   newDirectory.create(recursive: true).then((value) {
-//     if(value.existsSync()){
-//       print("Directory successfully !");
-//     }
-//   });
+//   Directory newDirectory = Directory("$pathGlobalDir/datas");
+//   newDirectory.create();
 // }
 
 
 //task 4
 // void main(){
-//   String pathLocalDir = "C:\\Users\\User\\IdeaProjects\\myhomtask\\assets\\dart";
-//   String fileName = "readme.md";
-//   File file = File("$pathLocalDir\\$fileName");
+//   File file = File("$pathGlobalDir\\$fileName");
 //   file.create();
 // }
 
 
 
-// task 5 / 6
+// task 5
 // void main()async{
-//   String pathLocalDir = "C:\\Users\\User\\IdeaProjects\\myhomtask\\assets\\dart";
-//   String fileName = "readme.md";
-//   File file = File("$pathLocalDir\\$fileName");
-//   await file.writeAsString("Salom Flutter B 28 Alibaba 303 hona");
-//   await file.writeAsString("odam");
+//   File file = File("$pathGlobalDir\\$fileName");
+//   await file.writeAsString("Birinchi Malumot").then((value) => prints("Birinchi ma'lumot yozildi !"));
 // }
 
+// Task - 6
+
+// void main() async{
+//   File file = File("$pathGlobalDir\\$fileName");
+//   await file.writeAsString("Ikkinchi ma'lumot").then((value) => print("Ma'lumot o'zgartitildi !"));
+// }
 
 // task 7
-// void main(){
-//   String pathLocalDir = "assets\\dart";
-//   Directory newDirectory = Directory(pathLocalDir);
-//   newDirectory.rename("dart").then((value) {
-//          if(value.existsSync()){
-//            print("Directory successfully !");
-//          }
-//        });
+// void main()async{
+//   Directory newDirectory = Directory(pathGlobalDir);
+//   File file = File("${newDirectory.path}\\$fileName");
+//   // await Directory("C:\\Users\\abdu7\\OneDrive\\Desktop\\dart\\my_home_task\\bin\\assets").create(recursive: true);
+//   File file2 = await file.copy("C:\\Users\\abdu7\\OneDrive\\Desktop\\dart\\my_home_task\\bin\\assets\\$fileName");
 // }
 
 // task 8
@@ -75,8 +73,13 @@ import 'dart:io';
 
 //task 9
 // void main() async {
-//   String pathLocalDir = "C:\\Users\\User\\IdeaProjects\\myhomtask\\assets";
 //   String fileName = "readme.md";
-//   File file = File("$pathLocalDir\\$fileName");
+//   File file = File("$pathGlobalDir\\$fileName");
 //   print(await  file.readAsString());
+// }
+
+// Task - 10
+//
+// void main() {
+//   Directory(pathGlobalDir).delete(recursive: true).then((value) => prints("O'chirildi datas"));
 // }
